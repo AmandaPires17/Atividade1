9370bcc6937b41adc1c36e25d6e3a797c8b7247a
AmandaPires17 <102668847+AmandaPires17@users.noreply.github.com>
2022-04-14 11:45:48 -0300

    Add files via upload

---
+++ src/App.vue
@@ -0,0 +1,24 @@
+<script></script>
+ <template>
+   <h1>Botões Coloridos</h1>
+
+   <button id="btnVerde">Verde</button>
+  
+   <button id="btnAzul">Azul</button>
+ </template>
+
+ <style>
+
+   #btnVerde {
+     background-color: rgb(118, 206, 118);
+     color: rgb(37, 101, 6);
+     width: 90px;
+     height: 50px;
+     border-radius: 6px;
+     margin: 30px;
+     font-size: 20px;
+   }
+ 
+ </style>
+
+
